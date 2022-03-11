cbd135d2ed571541a0715f9c13a4f7aecf94c4ce
carlos pizarro <cpizarroal@gmail.com>
2022-03-10 23:50:26 -0300

    padding top de section modificado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,17 @@
 //mensaje de ofertas
 alert("Oferta 2x1!!!!!!")
+
+// componente toast
+$(document).ready(function() {
+
+
+    var toastTrigger = document.getElementById('liveToastBtn')
+    var toastLiveExample = document.getElementById('liveToast')
+    if (toastTrigger) {
+        toastTrigger.addEventListener('click', function() {
+            var toast = new bootstrap.Toast(toastLiveExample)
+
+            toast.show()
+        })
+    }
+});
